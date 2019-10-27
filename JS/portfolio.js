5db0fa5fdf83aea675442256f09299605f027611
Cameron <hackwithcameron@gamil.com>
2019-10-26 21:38:33 -0700

    Updated entire portfolio
    added popup contact window. new background. intigrated javascript and jQuery

---
+++ JS/portfolio.js
@@ -0,0 +1,38 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+  
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+} 
+
+
+
+
+
+
+
+
+
+/* jQuery */
+
+/*-----------SLIDESHOW------------*/
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+    $("#slideshow > div:first")
+        .fadeOut(1000)
+        .next()
+        .fadeIn(1000)
+        .end()
+        .appendTo("#slideshow");
+}, 3000);
+
+$("#enter").click(function() {
+    $('html, body').animate( {
+        scrollTop: $(".background2").offset().top
+    }, 500);
+});
+
+
